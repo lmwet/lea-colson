cbb3b1406c051a80235a30c4e7fc81ce7aea9656
Lmwet <lea.colson@gmail.com>
2020-12-07 13:21:47 +0100

    edited contact page

--- src/components/me.js
+++ src/components/me.js
@@ -1,35 +1,49 @@
 import React from "react";
 
 export default function Me() {
-    return (
-        <React.Fragment>
-            <div id="contactme" className="footer-blurb">
-                <h1 className="page-header">Contact</h1>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-4 footer-blurb-item">
-                            <a href="mailto:lea.colson@gmail.com">
-                                <img
-                                    alt="lmwet"
-                                    id="lmwet"
-                                    className="img-responsive"
-                                    src="/images/lmwet.png"
-                                />
-                            </a>
-                        </div>
-                        <div className="col-sm-4 footer-blurb-item">
-                            <a href="mailto:lea.colson@gmail.com">
-                                <p>Send me an email</p>
-                            </a>
-                            <a href="tel:0049015163050145">
-                                <p>Or give me a call: +49 015163050145</p>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div className="white-space"></div>
-                </div>
+  return (
+    <React.Fragment>
+      <div id="contactme" className="footer-blurb">
+        <h1 className="page-header">Contact</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4 footer-blurb-item">
+              <a href="mailto:lea.colson@posteo.net">
+                <img
+                  alt="lmwet"
+                  id="lmwet"
+                  className="img-responsive"
+                  src="/images/lmwet.png"
+                />
+              </a>
             </div>
-        </React.Fragment>
-    );
+            <div id="black-blob" className="col-sm-4 footer-blurb-item">
+              <a href="/images/CV_Colson_2020.pdf" download="CV_Lea_Colson">
+                <p id="download"> Download my CV</p>
+              </a>
+            </div>
+
+            <div id="white-blob" className="col-sm-4 footer-blurb-item">
+              <a href="mailto:lea.colson@posteo.net">
+                <span>Send me an email</span>
+              </a>
+            </div>
+          </div>
+          <div id="black-blob" className="col-sm-4 footer-blurb-item">
+            <a href="tel:004915163050145">
+              <p id="download">Or give me a call</p>
+              <p id="download">+49 15163050145</p>
+            </a>
+          </div>
+
+          <div className="white-space"></div>
+          <div className="white-space"></div>
+          <div className="white-space"></div>
+          <div className="white-space"></div>
+          <div className="white-space"></div>
+          <div className="white-space"></div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
