cf205908462027911435148c698d5f4ce0c8b774
Lmwet <lea.colson@gmail.com>
2020-12-08 15:13:25 +0100

    added parallax to contact page

--- src/components/me.js
+++ src/components/me.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Parallax } from "react-scroll-parallax";
 
 export default function Me() {
   return (
@@ -8,34 +9,49 @@ export default function Me() {
         <div className="container">
           <div className="row">
             <div className="col-sm-4 footer-blurb-item">
-              <a href="mailto:lea.colson@posteo.net">
-                <img
-                  alt="lmwet"
-                  id="lmwet"
-                  className="img-responsive"
-                  src="/images/lmwet.png"
-                />
-              </a>
+              <Parallax className="white-blob" x={[-135, 75]} tagOuter="figure">
+                <a href="mailto:lea.colson@posteo.net">
+                  <img
+                    alt="lmwet"
+                    id="lmwet"
+                    className="img-responsive"
+                    src="/images/lmwet.png"
+                  />
+                </a>
+              </Parallax>
             </div>
+
+            <Parallax className="white-blob" x={[-25, 55]} tagOuter="figure">
+              <div id="black-blob" className="col-sm-4 footer-blurb-item">
+                <a href="/images/CV_Colson_2020.pdf" download="CV_Lea_Colson">
+                  <p id="download"> Download my CV</p>
+                </a>
+              </div>
+            </Parallax>
+
+            <Parallax className="white-blob" x={[175, -55]} tagOuter="figure">
+              <div id="white-blob" className="col-sm-4 footer-blurb-item">
+                <a href="mailto:lea.colson@posteo.net">
+                  <span>Send me an email</span>
+                </a>
+              </div>
+            </Parallax>
+          </div>
+          <Parallax className="white-blob" x={[175, -55]} tagOuter="figure">
             <div id="black-blob" className="col-sm-4 footer-blurb-item">
-              <a href="/images/CV_Colson_2020.pdf" download="CV_Lea_Colson">
-                <p id="download"> Download my CV</p>
+              <a href="tel:004915163050145">
+                <p id="download">Or give me a call</p>
+                <p id="download">+49 15163050145</p>
               </a>
             </div>
+          </Parallax>
 
-            <div id="white-blob" className="col-sm-4 footer-blurb-item">
-              <a href="mailto:lea.colson@posteo.net">
-                <span>Send me an email</span>
-              </a>
-            </div>
-          </div>
-          <div id="black-blob" className="col-sm-4 footer-blurb-item">
-            <a href="tel:004915163050145">
-              <p id="download">Or give me a call</p>
-              <p id="download">+49 15163050145</p>
-            </a>
-          </div>
-
+          <div className="white-space"></div>
+          <div className="white-space"></div>
+          <div className="white-space"></div>
+          <div className="white-space"></div>
+          <div className="white-space"></div>
+          <div className="white-space"></div>
           <div className="white-space"></div>
           <div className="white-space"></div>
           <div className="white-space"></div>
